c2e84f542b69fdb8a6121be7f1eeb4479f384f2f
Fernando <fernandopereira-3@hotmail.com>
2025-01-01 08:32:57 -0300

    Readme and Frontend JS update

--- frontend/js.js
+++ frontend/js.js
@@ -5,7 +5,7 @@ $("#button-blue").on("click", function() {
     var txt_comentario = $("#comment").val();
 
     $.ajax({
-        url: "",
+        url: "127.0.0.1:8080",
         
         type: "post",
         data: {nome: txt_nome, comentario: txt_comentario, email: txt_email},
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Serie de desafios do site DIO.ME
+
+Series of challenges from the Dio.me website. 
+
+Creating a complete deployment of an app in PHP, this script can be used both for local use with MiniKube and in real production on one of the available Clouds.
